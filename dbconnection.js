ba132e971e18a27f2ddaafe58d6b41d5828310e3
mingokl02135 <mingbudsakorn@gmail.com>
2019-11-21 17:12:24 +0700

    change query data

--- dbconnection.js
+++ dbconnection.js
@@ -3,7 +3,7 @@ var connection = mysql.createPool({
   host: 'localhost',
   // port: '3306',
   user: 'root',
-  password: 'root',
+  password: 'ming',
   database: 'showhuai',
   insecureAuth: true
 });
