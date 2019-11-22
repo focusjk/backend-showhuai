c9d8e359cb9f00f9fcce9adac56d16b420e42824
JaneA <janejira885@gmail.com>
2019-11-22 15:29:07 +0700

    Create get car, invoice and messenger functions for Sending page

--- models/sendinground.js
+++ models/sendinground.js
@@ -5,13 +5,30 @@ const getAllRound = (callback) => {
     return db.query('SELECT * FROM Sending_round', callback);
 }
 
-const deleteByID = (srID, callback) => {
-    console.log('delete sending round id: = ', srID)
-    return db.query('DELETE FROM Sending_round WHERE ID=?', srID, callback );
+const getCar = (callback) => {
+    console.log('get all cars')
+    return db.query('SELECT * FROM Car', callback);
 }
 
-const addRound = (srID,depart,arrive,msg,plate,callback) => {
-    console.log('add new round id: = ',srID)
+const getMessenger = (callback) => {
+    console.log('get all messenger')
+    return db.query('SELECT * FROM Messenger', callback);
+}
+
+const getInvoice = (callback) => {
+    console.log('get all invoice with null sending round')
+    return db.query(`select * from Invoice
+    inner join Member_invoice on Invoice.ID =  Member_invoice.ID 
+    where Sending_round_ID is null;`, callback);
+}
+
+const deleteByID = (srID, callback) => {
+    console.log('delete sending round id: = ', srID)
+    return db.query('DELETE FROM Sending_round WHERE ID=?', srID, callback);
+}
+
+const addRound = (srID, depart, arrive, msg, plate, callback) => {
+    console.log('add new round id: = ', srID)
     return db.query("INSERT INTO Sending_round VALUES (" + srID + ',"' + depart + '","' + arrive + '","' + msg + '","' + plate + '")', callback);
 }
 
@@ -21,4 +38,4 @@ const addRound = (srID,depart,arrive,msg,plate,callback) => {
 // "Messenger_SSN": "1234512345123",
 // "License_plate": "AB1234"
 
-module.exports = {getAllRound,deleteByID,addRound};
+module.exports = { getAllRound, deleteByID, addRound, getCar, getMessenger, getInvoice };
--- routes/sendinground.js
+++ routes/sendinground.js
@@ -22,27 +22,66 @@ router.get('/', function (req, res, next) {
     })
 });
 
+router.get('/messenger', function (req, res, next) {
+    sendingModel.getMessenger((err, result) => {
+        if (err) {
+            res.json(err);
+        }
+        else {
+            console.log(result)
+            res.json(result);
+        }
+    })
+});
+
+router.get('/car', function (req, res, next) {
+    sendingModel.getCar((err, result) => {
+        if (err) {
+            res.json(err);
+        }
+        else {
+            console.log(result)
+            carList = result.map(i => i.License_plate)
+            console.log(carList)
+            res.json(carList);
+        }
+    })
+});
+
+router.get('/invoice', function (req, res, next) {
+    sendingModel.getInvoice((err, result) => {
+        if (err) {
+            res.json(err);
+        }
+        else {
+            console.log(result)
+            res.json(result);
+        }
+    })
+});
+
+
 router.delete('/del', function (req, res, next) {
     const { ID } = req.body
     console.log(ID)
     const newID = ID ? ID : ''
-
     sendingModel.deleteByID(newID, (err, result) => {
         if (err) {
             res.json(err);
         }
         else {
             res.json(result);
+
         }
 
     });
 });
 
 router.put('/add', function (req, res, next) {
-    const {ID,Depart_time,Arrive_time,Messenger_SSN,License_plate} = req.body
+    const { ID, Depart_time, Arrive_time, Messenger_SSN, License_plate } = req.body
     //console.log(ID,Depart_time,Arrive_time,Messenger_SSN,License_plate)
 
-    sendingModel.addRound(ID,Depart_time,Arrive_time,Messenger_SSN,License_plate, (err, result) => {
+    sendingModel.addRound(ID, Depart_time, Arrive_time, Messenger_SSN, License_plate, (err, result) => {
         if (err) {
             res.json(err);
         }
